a66626e6547622455f3bf993fe3f38872eef1d3e
ZimIT32 <ZimIT32@gmail.com>
2022-03-07 09:46:46 -0800

    Added Webserver with HTML page

--- 002/main.tf
+++ 002/main.tf
@@ -21,8 +21,27 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+          #!/bin/bash
+          echo "Hello World" > index.html
+          nohup busybox httpd -f -p 8080 &
+          EOF
 
   tags = {
-    Name = "ubunut_micro_001"
+    Name = "ubunut_webserver_001"
+  }
+}
+
+
+resource "aws_security_group" "instance" {
+  name = "ubuntu-webserver-test"
+
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
